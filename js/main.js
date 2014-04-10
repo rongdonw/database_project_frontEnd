16fd181ad82be718024cd743187344f719a01784
Lucy <peachie.monkey@gmail.com>
2014-04-09 20:59:11 -0400

    cleaned up staff page

--- js/main.js
+++ js/main.js
@@ -233,6 +233,8 @@ function populateStaff(){
         if(http.readyState == 4){
             // Assign the returned value to the document object.
             document.getElementById('sid_dropdown').innerHTML = http.responseText;
+            if (document.getElementById('search_hall_dropdown') != null)
+                document.getElementById('search_hall_dropdown').innerHTML = http.responseText;
         }
     }
     http.send(null);
